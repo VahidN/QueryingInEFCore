8228f582071919c0f2a62b65923cdf23fd9e27e8
Vahid Nasiri <vahid_nasiri@yahoo.com>
2020-07-29 11:14:12 +0430

    Add DeleteAMemberFromTheMembersTable.Test_Method2

--- Exercises/ModifyingData/DeleteAMemberFromTheMembersTable.cs
+++ Exercises/ModifyingData/DeleteAMemberFromTheMembersTable.cs
@@ -2,6 +2,8 @@ using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using EFCorePgExercises.DataLayer;
 using EFCorePgExercises.Utils;
+using EFCorePgExercises.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace EFCorePgExercises.Exercises.ModifyingData
 {
@@ -9,7 +11,7 @@ namespace EFCorePgExercises.Exercises.ModifyingData
     public class DeleteAMemberFromTheMembersTable
     {
         [FullyQualifiedTestMethod, Ignore]
-        public void Test()
+        public void Test_Method1()
         {
             // https://pgexercises.com/questions/updates/deletewh.html
             // We want to remove member 37, who has never made a booking, from our database. How can we achieve that?
@@ -21,6 +23,28 @@ namespace EFCorePgExercises.Exercises.ModifyingData
                 context.Members.Remove(mem37);
 
                 context.SaveChanges();
+            });
+        }
+
+        [FullyQualifiedTestMethod, Ignore]
+        public void Test_Method2()
+        {
+            // https://pgexercises.com/questions/updates/deletewh.html
+            // We want to remove member 37, who has never made a booking, from our database. How can we achieve that?
+            // delete from cd.members where memid = 37;
+
+            EFServiceProvider.RunInContext(context =>
+            {
+                var entry = context.Entry(new Member { MemId = 37 });
+                entry.State = EntityState.Deleted;
+                context.SaveChanges();
+
+                /*
+                SET NOCOUNT ON;
+                DELETE FROM [Members]
+                WHERE [MemId] = @p0;
+                SELECT @@ROWCOUNT;
+                */
             });
         }
     }
